f6a1bf7bca0e601a68cf46546d1fb278cd9506b1
Iffat Hossain <iffathossain77@gmail.com>
2023-08-25 23:36:27 -0400

    intro / fundamentals

---
+++ go-fundamentals/intro.go
@@ -0,0 +1,57 @@
+package main 
+
+import "fmt"
+
+func main(){
+
+	// var a int = 5
+	// var b float32 = 4.32
+	// const pi float64 = 3.1415139475
+
+	// //shorthand for declaring and initializing a variable,
+	// x,y := 14,15
+
+	// fmt.Println(a)
+	// fmt.Println(b)
+	// fmt.Println(pi)
+	// fmt.Println(x , " ,", y)
+
+
+	// arithmetic  operators
+
+	// x,y := 5,6
+
+	// fmt.Println("x + y = ", x + y)
+	// fmt.Println("x - y = ", x - y)
+	// fmt.Println("x * y = ", x * y)
+	// fmt.Println("x / y = ", x / y)
+	// fmt.Println("x mod y = ", x % y)
+
+	// // boolean operators
+	// isbool := true
+	// hate := false
+
+	// fmt.Println(isbool && hate)
+	// fmt.Println(isbool || hate)
+	// fmt.Println(!isbool)
+
+
+
+// -----------------------------------------------
+
+
+	//using pointers
+
+	// x:= 10 
+	// fmt.Println(&x)
+	// changeValue(&x)
+	// fmt.Println(x)
+	// // // & infront of the value here allows you to print the memory location
+	// fmt.Println(&x)
+	
+	// func changeValue(x *int) {
+	
+	// 	*x = 7
+	// }
+
+}
