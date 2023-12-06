a6365d45715309add9672bb13e9050012ae85b36
Iffat Hossain <iffathossain77@gmail.com>
2023-12-06 12:05:02 -0500

    completes arrays, adds structs

--- go-fundamentals/arrays/sum.go
+++ go-fundamentals/arrays/sum.go
@@ -1,20 +1,18 @@
 package main
 
-
-
 func Sum(numbers []int) int {
 	sum := 0
 
 	for _, number := range numbers {
 		sum += number
 	}
-	return sum 
+	return sum
 }
 
 // func SumAll(arr1 []int, arr2 [] int) []int {
 // 	sumArr := [] int {}
 // 	tot1 := 0
-// 	tot2 := 0 
+// 	tot2 := 0
 
 // 	for _, num := range arr1 {
 // 		tot1 += num
@@ -26,8 +24,7 @@ func Sum(numbers []int) int {
 // 	sumArr = append(sumArr,tot1, tot2)
 
 // 	return sumArr
-// }   
-
+// }
 
 // chapter example
 // func SumAll(numbersToSum ...[]int) []int {
@@ -35,7 +32,7 @@ func Sum(numbers []int) int {
 // 	lengthOfNumbers := len(numbersToSum)
 // 	// sums will ba a slice that will have a size of lengthOfNumbers
 // 	sums := make([]int, lengthOfNumbers)
-//   // looping over numbers to sum wich are slices and calling Sum to get the sum 
+//   // looping over numbers to sum wich are slices and calling Sum to get the sum
 // 	//  of each nested slice, then assigning that sum to sums at the index equal to the index of the numbersToSum slice
 // 	for i, numbers := range numbersToSum {
 // 		sums[i] = Sum(numbers)
@@ -53,13 +50,12 @@ func Sum(numbers []int) int {
 // 	return sums
 // }
 
-
-func SumAllTails(numbersToSum ...[]int) []int { 
+func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 	for _, numbers := range numbersToSum {
 		if len(numbers) == 0 {
 			sums = append(sums, 0)
-		}else {
+		} else {
 			tail := numbers[1:]
 			sums = append(sums, Sum(tail))
 		}
--- go-fundamentals/arrays/sum_test.go
+++ go-fundamentals/arrays/sum_test.go
@@ -5,20 +5,19 @@ import (
 	"testing"
 )
 
-func TestSum(t *testing.T){
- 
-	t.Run("collection of 5 numbers", func(t *testing.T){
-	numbers := []int{1, 2 ,3 ,4 ,5}
+func TestSum(t *testing.T) {
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
 
 	})
-
 
 }
 
